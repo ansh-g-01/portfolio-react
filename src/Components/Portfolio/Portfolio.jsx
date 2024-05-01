42f2adf368c4a00749033872cc86d24ba952af16
Ansh <ansh21000@gmail.com>
2024-05-01 12:52:40 +0530

    made project links functional

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import './Portfolio.css'
+import React from 'react';
+import './Portfolio.css';
 
 const Portfolio = () => {
   return (
-    <div className='Projects ' id='Projects'>
+    <div className='Projects' id='Projects'>
       <h1>Projects</h1>
 
       <fieldset className='names'>
-        <h1><span>1.Python BCD Code Converter</span></h1>
+        <a href="https://colab.research.google.com/drive/1O6qiI7DE0pMc0yAtCbw3lw6wTHgO7kKb" target="_blank" rel="noopener noreferrer">
+          <h1><span>1. Python BCD Code Converter</span></h1>
+        </a>
         <p></p>
       </fieldset>
-      <fieldset className='names' id='2'>
-        <h1><span>2.GPA Calculator</span></h1>
+      <fieldset className='names' >
+       
+          <h1><span>2. GPA Calculator</span></h1>
+      
         <p></p>
       </fieldset>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;

32a667d30440c0efc7b26dddc2e8683769ade2b7
Ansh <ansh21000@gmail.com>
2024-05-01 13:25:16 +0530

    added information on projects

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -10,13 +10,18 @@ const Portfolio = () => {
         <a href="https://colab.research.google.com/drive/1O6qiI7DE0pMc0yAtCbw3lw6wTHgO7kKb" target="_blank" rel="noopener noreferrer">
           <h1><span>1. Python BCD Code Converter</span></h1>
         </a>
-        <p></p>
-      </fieldset>
+        <p>-This project takes BCD code inputs and devises expressions for conversion in standarad binary. These expressions are executed in behavioural, dataflow and structural level verilog codes</p>
+         <p>-Devised complex algorithms to construct Karnaugh Maps and synthesize simplified Boolean expressions, significantly
+            enhancing efficiency in data flow and behavioural level Verilog code development</p>
+                </fieldset>
       <fieldset className='names' >
        
           <h1><span>2. GPA Calculator</span></h1>
       
-        <p></p>
+        <p>-Developed and designed a user-friendly GPA calculator web application, enhancing student
+interaction through intuitive HTML/CSS interfaces</p>
+<p>-Implementing dynamic JavaScript for real-time validation and GPA computation,
+demonstrating advanced proficiency in client-side scripting</p>
       </fieldset>
     </div>
   );

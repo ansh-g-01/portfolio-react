18915e3bf4e5de8979dd11f0bd45f89924699b20
ansh-g-01 <123054397+ansh-g-01@users.noreply.github.com>
2024-10-03 18:37:45 +0530

    Update Hero.jsx
    updated 3 rd year

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt=""  style={{ width: '300px', height: '330px'    }}/>
-      <h1><span>I'm Ansh Gandhi,</span> Anchor and Tech Enthusiast</h1>
-      <p>I am studying in 2nd year VIT Vellore. I have hosted Riviera Proshows,South Asia's One of the largest cultural fest</p>
+      <h1><span>I'm Ansh Gandhi,</span> Tech Enthusiast and Anchor</h1>
+      <p>I am studying in 3rd year VIT Vellore. I have hosted Riviera Proshows,South Asia's One of the largest cultural fest</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link'  offset={50} href='#contact'>Connect with me</AnchorLink></div>
         <div className="hero-resume"><a href={resume} target='blank'>My resume</a></div>
